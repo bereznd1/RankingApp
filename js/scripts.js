8efa8b0ecf976c0b071c95f4e39e405839fb33ac
Dennis <bereznd1@tcnj.edu>
2019-09-25 20:46:37 -0400

    basic logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,25 @@
-$("#submit-raw-list").on("click", function (e) {
-    //e.preventDefault();
-    console.log('whatup');
+
+$(document).ready(function () {
+
+    var itemsEntered = [];
+
+    $("#submit-raw-list").on("click", function (e) {
+        e.preventDefault();
+
+
+        $("input[name='item']").each(function (index, value) {
+
+            itemsEntered.push($(this).val());
+
+        });
+
+        $(".form-entry").hide();
+
+        $(".main-wrap").append($("<div class='questions'></div>"));
+
+        console.log(itemsEntered);
+
+    });
+
 });
 
-console.log('whatup');
