b387a4c60a5ef3b872485ee98fdc88d4c1fad84d
Dennis <bereznd1@tcnj.edu>
2019-09-25 22:18:21 -0400

    on submit logic start

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
 
 $(document).ready(function () {
 
+
+    function selectChoice() {
+        // $('.selection').on("click", function (e) {
+        $(this).closest('p').hide();
+        $(this).hide();
+        console.log($(this));
+        // });
+    }
+
+
+
+
     var itemsEntered = [];
 
     $("#submit-raw-list").on("click", function (e) {
         e.preventDefault();
 
+
+        // MAKE IT SO IF ANY FIELDS ARE LEFT BLANK, IT DOESNT GO THRU, GIVES ERROR MESSAGE
+        // ADD BACK BUTTON AFTER THEY HAVE SUBMITTED
 
         $("input[name='item']").each(function (index, value) {
 
@@ -17,9 +32,39 @@ $(document).ready(function () {
 
         $(".main-wrap").append($("<div class='questions'></div>"));
 
+
+
+
+        for (i = 0; i < itemsEntered.length; i++) {
+
+            if (i == 0) {
+                continue;
+
+
+            }
+
+            else {
+                //console.log(itemsEntered[i] + "vs" + itemsEntered[i - 1])
+                $(".questions").append("<p><a class='selection' onclick='selectChoice()'>" + itemsEntered[i] + "</a> vs. <a class='selection'>" + itemsEntered[i - 1] + "</a></p>");
+                return;
+
+            }
+
+
+        }
+
+
+
+
+
+
+
         console.log(itemsEntered);
 
     });
+
+
+
 
 });
 
